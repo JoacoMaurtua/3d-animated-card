fcf054dfe16e781a00bb1299085e5a19bf77a301
JoacoMaurtua <joacomaurtua@gmail.com>
2021-08-11 21:34:18 -0500

    create the styled card

---
+++ 3d-animated-card/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+import NikeCard from './components/nikeCard';
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10rem;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <NikeCard />
+    </AppContainer>
+      
+  );
+}
+
+export default App;
---
+++ 3d-animated-card/src/components/nikeCard/shoesDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function shoesDetails() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
